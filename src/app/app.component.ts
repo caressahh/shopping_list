5098e2803894e65a42380f4e08687d0b23c52741
Carissa <carissa@edgren.nu>
2017-08-07 17:33:35 +0200

    Able to add items to shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shopping List';
+  newWine = { name:'', price:0 };
   shoppingList = [
   {name: "white wine", price: 1.39},
   {name: "red wine", price: 12.39},
   {name: "blue wine", price: 6.39}
   ]
+  addItem(){
+    this.shoppingList.push(this.newWine)
+    this.newWine = {name:'', price:0}
+  }
 }
