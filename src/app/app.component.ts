9363787c21cece7ab0b84d3e7e2b540d35166088
Carissa <carissa@edgren.nu>
2017-08-08 16:32:03 +0200

    implemented amount and total

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shopping List';
-  newWine = { name:'', price:0 };
+  newWine = { name:'', price:0, amount:0 };
   shoppingList = [
-  {name: "white wine", price: 1.39},
-  {name: "red wine", price: 12.39},
-  {name: "blue wine", price: 6.39}
-  ]
+  {name: "white wine", price: 1.39, amount:1},
+  {name: "red wine", price: 12.39, amount:1},
+  {name: "blue wine", price: 6.39, amount:1}
+  ];
+
+  totalPrice = 0;
+
+  calculateTotal(){
+    this.shoppingList.forEach(wine=>{
+      this.totalPrice += (wine.price * wine.amount);
+    });
+  }
+
   addItem(){
     this.shoppingList.push(this.newWine)
-    this.newWine = {name:'', price:0}
+    this.newWine = {name:'', price:0, amount:0}
+    this.calculateTotal();
+  }
+
+  ngAfterViewInit() {
+    this.calculateTotal();
   }
 }
